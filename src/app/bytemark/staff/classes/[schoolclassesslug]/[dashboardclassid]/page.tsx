a77437be81db3acba76ca416dbca386a2a9dc218
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-11-05 20:22:08 +0000

    mvp

--- src/app/bytemark/staff/classes/[schoolclassesslug]/[dashboardclassid]/page.tsx
+++ src/app/bytemark/staff/classes/[schoolclassesslug]/[dashboardclassid]/page.tsx
@@ -15,15 +15,17 @@ interface StudentDataItem {
     questionmarks: number;
     question_id: number;
     subtopic_id: number;
+    subtopic_number: string;
     subtopic_title: string;
     course_level: string;
     unit_id: number;
     unit_title: string;
     topic_id: number;
-    topic_number: number;
+    topic_number: string;
     topic_title: string;
     subjectname: string;
     examboard: string;
+    unit_number: string;
 }
 
 interface CourseAggregatedDataItem {
@@ -33,17 +35,47 @@ interface CourseAggregatedDataItem {
     total_marks_achieved: number;
     total_marks_available: number;
     percentage: number;
+
 }
 
 interface UnitAggregatedDataItem {
     student_id: number;
     unit_id: number;
-    unittitle: number;
+    unit_title: string;
     first_name: string;
     last_name: string;
     total_marks_achieved: number;
     total_marks_available: number;
     percentage: number;
+    unit_number: string;
+}
+
+interface TopicAggregatedDataItem {
+    student_id: number;
+    unit_title: string;
+    first_name: string;
+    last_name: string;
+    total_marks_achieved: number;
+    total_marks_available: number;
+    percentage: number;
+    unit_number: string;
+    topic_number: string;
+    topic_title: string;
+}
+
+interface SubTopicAggregatedDataItem {
+    student_id: number;
+    unit_title: string;
+    first_name: string;
+    last_name: string;
+    subtopic_title: string;
+    subtopic_number: string;
+    total_marks_achieved: number;
+    total_marks_available: number;
+    percentage: number;
+    unit_number: string;
+    topic_number: string;
+    topic_title: string;
 }
 
 export default async function DashboardClasses({ params }: { params: { dashboardclassid: string } }) {
@@ -164,10 +196,94 @@ export default async function DashboardClasses({ params }: { params: { dashboard
         return { student_id: student, first_name, last_name, total_marks_achieved, total_marks_available, percentage };
     });
 
-    // Step 3: Constructing the Table
+    // Identify unique units
+    const units = Array.from(new Set(studentData.map(item => item.unit_id)));
+
+    const unitAggregatedData: { [key: number]: UnitAggregatedDataItem[] } = {};
+    units.forEach(unit => {
+        unitAggregatedData[unit] = students.map(student => {
+            const studentUnitData = studentData.filter(item => item.student_id === student && item.unit_id === unit);
+            const first_name = studentUnitData[0]?.first_name || '';
+            const last_name = studentUnitData[0]?.last_name || '';
+            const unit_title = studentUnitData[0]?.unit_title || '';
+            const unit_number = studentUnitData[0]?.unit_number || '';
+            const total_marks_achieved = studentUnitData.reduce((total, item) => total + item.mark, 0);
+            const total_marks_available = studentUnitData.reduce((total, item) => total + item.questionmarks, 0);
+            const percentage = (total_marks_achieved / total_marks_available) * 100;
+            return { student_id: student, unit_id: unit, unit_number, unit_title, first_name, last_name, total_marks_achieved, total_marks_available, percentage };
+        });
+    });
+
+    const sortedUnits = units.sort((a, b) => {
+        const unitNumberA = unitAggregatedData[a][0]?.unit_number || '';
+        const unitNumberB = unitAggregatedData[b][0]?.unit_number || '';
+        return unitNumberA.localeCompare(unitNumberB, undefined, { numeric: true });
+    });
+
+    // Identify unique topics
+    const topics = Array.from(new Set(studentData.map(item => item.topic_id)));
+
+    // Aggregate data by topic
+    const topicAggregatedData: { [key: number]: TopicAggregatedDataItem[] } = {};
+    topics.forEach(topic => {
+        topicAggregatedData[topic] = students.map(student => {
+            const studentTopicData = studentData.filter(item => item.student_id === student && item.topic_id === topic);
+            const first_name = studentTopicData[0]?.first_name || '';
+            const last_name = studentTopicData[0]?.last_name || '';
+            const topic_title = studentTopicData[0]?.topic_title || '';
+            const unit_title = studentTopicData[0]?.unit_title || '';
+            const unit_number = studentTopicData[0]?.unit_number || '';
+            const topic_number = studentTopicData[0]?.topic_number || '';
+            const total_marks_achieved = studentTopicData.reduce((total, item) => total + item.mark, 0);
+            const total_marks_available = studentTopicData.reduce((total, item) => total + item.questionmarks, 0);
+            const percentage = (total_marks_achieved / total_marks_available) * 100;
+            return { student_id: student, unit_title, unit_number, topic_id: topic, topic_number, topic_title, first_name, last_name, total_marks_achieved, total_marks_available, percentage };
+        });
+    });
+
+    // Sort topics by topic_number
+    const sortedTopics = topics.sort((a, b) => {
+        const topicNumberA = topicAggregatedData[a][0]?.topic_number || '';
+        const topicNumberB = topicAggregatedData[b][0]?.topic_number || '';
+        return topicNumberA.localeCompare(topicNumberB, undefined, { numeric: true });
+    });
+
+
+    // Identify unique subtopics
+    const newSubtopics = Array.from(new Set(studentData.map(item => item.subtopic_id)));
+
+    // Aggregate data by subtopic
+    const subtopicAggregatedData: { [key: number]: SubTopicAggregatedDataItem[] } = {};
+    newSubtopics.forEach(subtopic => {
+        subtopicAggregatedData[subtopic] = students.map(student => {
+            const studentSubtopicData = studentData.filter(item => item.student_id === student && item.subtopic_id === subtopic);
+            const first_name = studentSubtopicData[0]?.first_name || '';
+            const last_name = studentSubtopicData[0]?.last_name || '';
+            const topic_title = studentSubtopicData[0]?.topic_title || '';
+            const unit_title = studentSubtopicData[0]?.unit_title || '';
+            const subtopic_title = studentSubtopicData[0]?.subtopic_title || '';
+            const subtopic_number = studentSubtopicData[0]?.subtopic_number || '';
+            const unit_number = studentSubtopicData[0]?.unit_number || '';
+            const topic_number = studentSubtopicData[0]?.topic_number || '';
+            const total_marks_achieved = studentSubtopicData.reduce((total, item) => total + item.mark, 0);
+            const total_marks_available = studentSubtopicData.reduce((total, item) => total + item.questionmarks, 0);
+            const percentage = (total_marks_achieved / total_marks_available) * 100;
+            return { student_id: student, unit_title, subtopic_title, subtopic_number, unit_number, topic_number, topic_title, first_name, last_name, total_marks_achieved, total_marks_available, percentage };
+        });
+    });
+
+    const sortedSubTopics = newSubtopics
+
+    // // Sort topics by topic_number
+    // const sortedSubTopics = newSubtopics.sort((a, b) => {
+    //     const subtopicNumberA = subtopicAggregatedData[a][0]?.subtopic_number || '';
+    //     const subtopicNumberB = subtopicAggregatedData[b][0]?.subtopic_number || '';
+    //     return subtopicNumberA.localeCompare(subtopicNumberB, undefined, { numeric: true });
+    // });
+
     return (
         <>
-
+            {/* Course Heading */}
             <div className="header-info bg-white p-4 rounded-md shadow-sm mb-4 border border-gray-300">
                 <h1 className="text-4xl mb-4 font-semibold">Dashboard: {classgroup}</h1>
                 <div className="text-lg mb-4">
@@ -175,8 +291,9 @@ export default async function DashboardClasses({ params }: { params: { dashboard
                     <p>Level: {level}</p>
                     <p>Exam Board: {examboard}</p>
                 </div>
-
             </div>
+
+            {/* Course Performance drilldown */}
             <div className="overflow-x-auto bg-white p-4 rounded-md shadow-sm mb-4 border border-gray-300">
                 <h2 className="text-2xl mb-4 font-semibold">Course Performance: {classgroup}</h2>
                 <div className="text-sm mb-4">
@@ -209,26 +326,124 @@ export default async function DashboardClasses({ params }: { params: { dashboard
                     </tbody>
                 </table>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Subtopic</th>
-                        {students.map(student => (
-                            <th key={student}>Student {student}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {aggregatedData.map((subtopicData, index) => (
-                        <tr key={index}>
-                            <td>{subtopicData.subtopic}</td>
-                            {students.map(student => (
-                                <td key={student}>{subtopicData[student]}</td>
+
+            {/* Unit Data Drill Down */}
+            <div className="overflow-x-auto bg-white p-4 rounded-md shadow-sm mb-4 border border-gray-300">
+                <h2 className="text-2xl mb-4 font-semibold">Unit Performance: {classgroup}</h2>
+                <div className="text-sm mb-4">
+                    <p>Subject: {subjectname}</p>
+                    <p>Level: {level}</p>
+                    <p>Exam Board: {examboard}</p>
+                </div>
+                {sortedUnits.map(unit => (
+                    <div key={unit} className="unit-table-container bg-white p-4 rounded-md shadow-sm mb-4 border border-gray-300">
+                        <h2 className="text-2xl mb-4 font-semibold">{unitAggregatedData[unit][0]?.unit_number || ''} - {unitAggregatedData[unit][0]?.unit_title || 'Unknown Unit'}</h2>
+                        <table className="min-w-full bg-white border-collapse text-sm">
+                            <thead className="bg-gray-200">
+                                <tr>
+                                    <th className="border px-2 py-1 font-bold">#</th>
+                                    <th className="border px-2 py-1 font-bold">First Name</th>
+                                    <th className="border px-2 py-1 font-bold">Last Name</th>
+                                    <th className="border px-2 py-1 font-bold">Total Marks Achieved</th>
+                                    <th className="border px-2 py-1 font-bold">Total Marks Available</th>
+                                    <th className="border px-2 py-1 font-bold">Percentage</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {unitAggregatedData[unit].map((item, index) => (
+                                    <tr key={index}>
+                                        <td className="border px-2 py-1">{item.student_id}</td>
+                                        <td className="border px-2 py-1">{item.first_name}</td>
+                                        <td className="border px-2 py-1">{item.last_name}</td>
+                                        <td className="border text-center px-2 py-1">{item.total_marks_achieved}</td>
+                                        <td className="border text-center px-2 py-1">{item.total_marks_available}</td>
+                                        <td className="border text-center px-2 py-1">{item.percentage.toFixed(2)}%</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                ))}
+            </div>
+
+            {sortedTopics.map(topic => (
+                <div key={topic} className="topic-table-container bg-white p-4 rounded-md shadow-sm mb-4 border border-gray-300">
+                    <h2 className="text-2xl mb-4 font-semibold">{topicAggregatedData[topic][0]?.topic_number || ''} - {topicAggregatedData[topic][0]?.topic_title || 'Unknown Topic'}</h2>
+                    <div className="text-sm mb-4">
+                        <p>{topicAggregatedData[topic][0]?.unit_number || ''} - {topicAggregatedData[topic][0]?.unit_title || 'Unknown Unit'}</p>
+                    </div>
+                    <div className="text-sm mb-4">
+                        <p>Subject: {subjectname}</p>
+                        <p>Level: {level}</p>
+                        <p>Exam Board: {examboard}</p>
+                    </div>
+                    <table className="min-w-full bg-white border-collapse text-sm">
+                        <thead className="bg-gray-200">
+                            <tr>
+                                <th className="border px-2 py-1 font-bold">#</th>
+                                <th className="border px-2 py-1 font-bold">First Name</th>
+                                <th className="border px-2 py-1 font-bold">Last Name</th>
+                                <th className="border px-2 py-1 font-bold">Total Marks Achieved</th>
+                                <th className="border px-2 py-1 font-bold">Total Marks Available</th>
+                                <th className="border px-2 py-1 font-bold">Percentage</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {topicAggregatedData[topic].map((item, index) => (
+                                <tr key={index}>
+                                    <td className="border px-2 py-1">{item.student_id}</td>
+                                    <td className="border px-2 py-1">{item.first_name}</td>
+                                    <td className="border px-2 py-1">{item.last_name}</td>
+                                    <td className="border text-center px-2 py-1">{item.total_marks_achieved}</td>
+                                    <td className="border text-center px-2 py-1">{item.total_marks_available}</td>
+                                    <td className="border text-center px-2 py-1">{item.percentage.toFixed(2)}%</td>
+                                </tr>
                             ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                        </tbody>
+                    </table>
+                </div>
+            ))
+            }
+            {/* Subtopics */}
+
+            {sortedSubTopics.map(subtopic => (
+                <div key={subtopic} className="subtopic-table-container bg-white p-4 rounded-md shadow-sm mb-4 border border-gray-300">
+                    <h2 className="text-2xl mb-4 font-semibold">{subtopicAggregatedData[subtopic][0]?.subtopic_number || ''} - {subtopicAggregatedData[subtopic][0]?.subtopic_title || 'Unknown Subtopic'}</h2>
+                    <div className="text-sm mb-4">
+                        <p>{subtopicAggregatedData[subtopic][0]?.topic_number || ''} - {subtopicAggregatedData[subtopic][0]?.topic_title || 'Unknown Topic'}</p>
+                        <p>{subtopicAggregatedData[subtopic][0]?.unit_number || ''} - {subtopicAggregatedData[subtopic][0]?.unit_title || 'Unknown Unit'}</p>
+                    </div>
+                    <div className="text-sm mb-4">
+                        <p>Subject: {subjectname}</p>
+                        <p>Level: {level}</p>
+                        <p>Exam Board: {examboard}</p>
+                    </div>
+                    <table className="min-w-full bg-white border-collapse text-sm">
+                        <thead className="bg-gray-200">
+                            <tr>
+                                <th className="border px-2 py-1 font-bold">#</th>
+                                <th className="border px-2 py-1 font-bold">First Name</th>
+                                <th className="border px-2 py-1 font-bold">Last Name</th>
+                                <th className="border px-2 py-1 font-bold">Total Marks Achieved</th>
+                                <th className="border px-2 py-1 font-bold">Total Marks Available</th>
+                                <th className="border px-2 py-1 font-bold">Percentage</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {subtopicAggregatedData[subtopic].map((item, index) => (
+                                <tr key={index}>
+                                    <td className="border px-2 py-1">{item.student_id}</td>
+                                    <td className="border px-2 py-1">{item.first_name}</td>
+                                    <td className="border px-2 py-1">{item.last_name}</td>
+                                    <td className="border text-center px-2 py-1">{item.total_marks_achieved}</td>
+                                    <td className="border text-center px-2 py-1">{item.total_marks_available}</td>
+                                    <td className="border text-center px-2 py-1">{item.percentage.toFixed(2)}%</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            ))}
         </>
     );
 }
